e67dc3595d8253a3fb0d70c768a68a22d4b0aaff
Kahiro-M <0204.kahiro@gmail.com>
2023-12-25 15:18:54 +0900

    v.1.0.0

---
+++ postcode2dic.py
@@ -0,0 +1,20 @@
+import pathlib
+import pandas as pd
+
+def toDic(inputFile):
+    outputFile = inputFile.rsplit('.', 1)[0] + '.csv'
+    zipcode = pd.read_csv(inputFile, header=None, dtype=str).fillna('').replace('nan','')
+    type = pd.DataFrame(['地名']*len(zipcode[2]))
+    dic = pd.DataFrame([
+        zipcode[2],
+        zipcode[6].replace('0','')+zipcode[7].replace('0','')+zipcode[8].replace('0',''),
+        type[0]
+        ],index=['zip','address','type']).transpose()
+    dic.to_csv('JpPostDic.txt', index=False, header=False, sep='\t')
+
+
+
+# 使用例
+inputFile = input("変換したいファイルのパスを入力してください: ")
+
+toDic(inputFile)
